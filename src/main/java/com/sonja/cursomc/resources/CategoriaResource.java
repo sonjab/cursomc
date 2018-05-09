263140f16452eea02511509b7a34317016fb6b43
Sonja Bitencourte <sonjabitencourte@gmail.com>
2018-05-08 22:15:25 -0300

    criacao da categoria

--- src/main/java/com/sonja/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sonja/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.sonja.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sonja.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-     @RequestMapping(method=RequestMethod.GET)
-	public String listar(){
-		return "REST OK!";
-		
-	}
 	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(2, "informatica");
+		Categoria cat2 = new Categoria(3, "escritorio");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
+	}
+
 }
